86573ee453432c448a3798c20d570087710b0c7a
veean <nersesowm@gmail.com>
2017-01-21 17:25:17 +0400

    made skeleton of challenge

--- cinemas.py
+++ cinemas.py
@@ -1,11 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+
+NOT_ARTHOUSE = 15
 
 
 def fetch_afisha_page():
-    pass
+    movie_info = requests.get('http://www.afisha.ru/msk/schedule_cinema/')
+    return movie_info.content
+
+
+def extract_casual_movies(movies_list):
+    casual_films = [film for film in movies_list if film['cinemas_count'] > NOT_ARTHOUSE]
+    return casual_films
 
 
 def parse_afisha_list(raw_html):
-    pass
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    raw_movies_info = soup.find_all('div', {'class': 'object s-votes-hover-area collapsed'})
+    movies_info_list = []
+
+    for movie in raw_movies_info:
+        movie_ = {'movie_title': movie.find('h3', class_='usetags').text, 'cinemas_count': len(movie.find('tbody'))}
+        movies_info_list.append(movie)
+
+    return extract_casual_movies(movies_info_list)
 
 
 def fetch_movie_info(movie_title):
